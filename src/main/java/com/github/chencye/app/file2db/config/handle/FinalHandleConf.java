de23d8c2c4eb1dc6868c67d11880ff3f22cd7409
chencye <chencye@gmail.com>
2017-09-28 23:18:29 +0800

    file2db

---
+++ src/main/java/com/github/chencye/app/file2db/config/handle/FinalHandleConf.java
@@ -0,0 +1,6 @@
+package com.github.chencye.app.file2db.config.handle;
+
+import com.github.chencye.app.file2db.config.FileHandleConf;
+
+public class FinalHandleConf extends FileHandleConf {
+}
---
+++ src/main/java/com/github/chencye/app/file2db/file/FileInfo.java
@@ -0,0 +1,9 @@
+package com.github.chencye.app.file2db.file;
+
+public class FileInfo {
+    private String path;
+    private String filename;
+
+
+
+}
